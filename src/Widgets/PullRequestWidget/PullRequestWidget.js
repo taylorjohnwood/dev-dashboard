f7a96f9b7fc1e003f3bce0587f6613ddc3e8d9a8
taylorjohnwood <taylorjohnwood@gmail.com>
2021-02-28 17:40:28 +1100

    Setup authentication

--- src/Widgets/PullRequestWidget/PullRequestWidget.js
+++ src/Widgets/PullRequestWidget/PullRequestWidget.js
@@ -1,5 +1,15 @@
 import { Box, Typography } from '@material-ui/core'
+import { createOAuthAppAuth } from '@octokit/auth-oauth-app';
 import React from 'react'
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+	authStrategy: createOAuthAppAuth,
+	auth: {
+		appId: "1a90f402b77ae117d0d5",
+		privateKey: "6e2e77fac9acd5dba28c4197aa481106a2e76696"
+	},
+})
 
 const PullRequestWidget = () => {
 
@@ -8,6 +18,13 @@ const PullRequestWidget = () => {
 		height: "100%",
 		padding: "5px"
 	}
+
+	console.log(octokit.pulls.get(
+		{
+			owner: "taylorjohnwood",
+			repo: "dev-dashboard"
+		}
+	))
 
 	return (
 		<Box style={widgetStyle}>
